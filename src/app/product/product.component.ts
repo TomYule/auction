ab9f769e1514423fb2e0b67c4bc2487f8bb8f514
hamming <brave.yhm@gmail.com>
2017-07-21 09:59:20 +0800

    星级 动态取值

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,19 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
   private products: Array<Product>;
+  private imgUrl = 'http://placehold.it/320x150';
 
   constructor() { }
 
   ngOnInit() {
     this.products =[
       new Product(1, "第一个商品", 1.99, 3.5, "这是第1个产品", ["电子产品","test"] ),
-      new Product(2,"第二个商品",1.99,3.5,"这是第2个产品",["test"]),
-      new Product(3,"第三个商品",1.99,3.5,"这是第3个产品",["test"]),
-      new Product(4,"第四个商品",1.99,3.5,"这是第4个产品",["test","test"]),
+      new Product(2,"第二个商品",1.99,4.5,"这是第2个产品",["test"]),
+      new Product(3,"第三个商品",1.99,1.5,"这是第3个产品",["test"]),
+      new Product(4,"第四个商品",1.99,2.5,"这是第4个产品",["test","test"]),
       new Product(5,"第五个商品",1.99,3.5,"这是第5个产品",["test","test"]),
-      new Product(6,"第六个商品",1.99,3.5,"这是第6个产品",["test","test"])
+      new Product(6,"第六个商品",1.99,4.5,"这是第6个产品",["test","test"])
     ]
-    this.products.push(new Product(7,"第七个商品",1.99,3.5,"这是第7个产品",["test","test"]));
+    // this.products.push(new Product(7,"第七个商品",1.99,3.5,"这是第7个产品",["test","test"]));
   }
 
 }
@@ -29,7 +30,7 @@ export class Product {
     public id: number,
     public title: string,
     public price: number,
-    public ratin: number,
+    public rating: number,
     public desc: string,
     public categories: Array<string>
   ) {
